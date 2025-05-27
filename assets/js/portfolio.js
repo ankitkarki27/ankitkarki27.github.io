df06a79b368263d11a65c880ddc448e84e4934f9
ankitkarki27 <ankitkarki8088@gmail.com>
2025-05-28 01:35:31 +0545

    next one

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -33,6 +33,7 @@ const projects = [
         description: "A full-stack Online Blood Donors Finder System built with PHP,Laravel, MySQL, HTML, CSS and Bootstrap.",
         tags: ["PHP", "Laravel", "MySQL"],
         github: "https://github.com/ankitkarki27/Donors-Nepal.git",
+        link: "https://github.com/ankitkarki27/Donors-Nepal.git",
 
     }
 ];
@@ -42,7 +43,8 @@ function createProjectHTML(project, index) {
     return `
             <div class="border-b border-gray-800 pb-8 animate-slide-up stagger-${index + 1} hover:border-gray-700 transition-colors">
                 <h3 class="text-xl font-semibold mb-3 text-white">${project.title}</h3>
-                <p class="text-gray-400 text-lg mb-4 leading-relaxed">
+                <p class="text-gray-400 mb-12 space-y-4 leading-relaxed">
+               
                     ${project.description}
                 </p>
                 <div class="flex text-m space-x-4">
